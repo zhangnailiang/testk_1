fd31661b3e1742445e2982f6ef8627eb59633f91
znlzbsbb@gmail.com <znlzbsbb@gmail.com>
2023-05-29 16:51:01 +1000

    test

---
+++ test_1.py
@@ -0,0 +1,9 @@
+print ("hello world")
+print(2 + 3)
+def main():
+    print('this program illustrate a chao')
+    x = eval(input("enter a number between a and 2"))
+    for i in range(10):
+        x = 3.9 * x * (1-x)
+        print(x)
+main()
